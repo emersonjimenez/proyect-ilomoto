b6f2fcf30ecae71a739b5ca0c85e666be5a892d2
emersonjimenez <jr21008@ues.edu.sv>
2025-01-14 23:20:08 -0600

    Subida de archivos spring boot

---
+++ src/main/java/com/motos/ilomoto/IlomotoApplication.java
@@ -0,0 +1,13 @@
+package com.motos.ilomoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlomotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlomotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motos/ilomoto/IlomotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motos.ilomoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlomotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
