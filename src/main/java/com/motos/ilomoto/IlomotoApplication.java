576993c26d339117e237335f88794eece153639b
emersonjimenez <jr21008@ues.edu.sv>
2025-01-14 23:20:08 -0600

    Subida de archivos spring boot

---
+++ src/main/java/com/motos/ilomoto/IlomotoApplication.java
@@ -0,0 +1,15 @@
+package com.motos.ilomoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlomotoApplication {
+
+	//Kevin estuvo aqui
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlomotoApplication.class, args);
+	}
+
+}
