6f9d0e8f096dd2d5073586784098afce7962b14a
AlexJG17 <kevinalexisjimenezruiz@gmail.com>
2025-01-16 15:46:45 -0600

    Subida de archivos spring boot

--- src/main/java/com/motos/ilomoto/IlomotoApplication.java
+++ src/main/java/com/motos/ilomoto/IlomotoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IlomotoApplication {
 
-	//Kevin estuvo aqui
+	//Kevin estuvo aqui.
 
 	public static void main(String[] args) {
 		SpringApplication.run(IlomotoApplication.class, args);
